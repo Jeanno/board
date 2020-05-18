0645bade46f7468ecfc154818c5d93620ae1fdbe
Jeanno <jeanno.cheung@gmail.com>
2020-05-18 17:23:13 -0400

    Default scope for Post

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,11 +1,21 @@
 'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
+  class Post extends Model {}
+
+  Post.init({
     content: {
       type: DataTypes.TEXT,
       allowNull: false
     }
-  }, {});
+  }, {
+    defaultScope: {
+      limit: 10,
+      order: [['id', 'DESC']]
+    },
+    sequelize,
+    modelName: 'Post'
+  });
   Post.associate = function(models) {
     // associations can be defined here
   };
